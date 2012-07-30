fe546f30843da04230c02d8d8631b0efaa140ade
Kate Gurieva <fleur.de.siel@gmail.com>
2012-07-30 13:31:08 +0300

    Initial Commit

---
+++ VAssistant/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VAssistant
+//
+//  Created by Gureva Ekaterina on 7/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UITabBarController *tabBarController;
+
+@end
---
+++ VAssistant/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  VAssistant
+//
+//  Created by Gureva Ekaterina on 7/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
